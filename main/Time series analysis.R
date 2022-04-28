ec8211c2a0cc1ae2d216cdb08f7d2d3be40eccda
Aidan Dunleavy <aidandunleavy00@gmail.com>
2022-04-27 19:00:30 -0500

    initial commit

---
+++ main/Time series analysis.R
@@ -0,0 +1,3 @@
+dataUSA = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv", header = TRUE)
+
+head(dataUSA)
